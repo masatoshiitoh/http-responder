9e8ec574becbbb3d1e1ae9e16632fa1ae77c8b19
Masatoshi Itoh <masatoshi9953@gmail.com>
2019-11-19 18:18:08 +0900

    changed: use web.router in main verticle.

--- src/main/java/jp/dressingroom/gameserver/apiguard/MainVerticle.java
+++ src/main/java/jp/dressingroom/gameserver/apiguard/MainVerticle.java
@@ -1,23 +1,48 @@
 package jp.dressingroom.gameserver.apiguard;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
+    HttpServer server = vertx.createHttpServer();
+
+    Router router = Router.router(vertx);
+
+    router.get("/").handler(getTopRoutingHandler());
+    router.get("/login").handler(getLoginRoutingHandler());
+
+    server.requestHandler(router).listen(8888);
+  }
+
+  private Handler<RoutingContext> getTopRoutingHandler() {
+    return routingContext -> {
+
+      // This handler will be called for every request
+      HttpServerResponse response = routingContext.response();
+      response.putHeader("content-type", "text/plain");
+
+      // Write to the response and end it
+      response.end("Hello World from Vert.x-Web!");
+    };
+  }
+
+  private Handler<RoutingContext> getLoginRoutingHandler() {
+    return routingContext -> {
+
+      // This handler will be called for every request
+      HttpServerResponse response = routingContext.response();
+      response.putHeader("content-type", "text/plain");
+
+      // Write to the response and end it
+      response.end("Login Handler from Vert.x-Web!");
+    };
   }
 }
