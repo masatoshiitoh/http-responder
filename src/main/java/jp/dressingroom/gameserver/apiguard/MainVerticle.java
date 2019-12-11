65c4d9bbc4f002074135817d495f15088f9b6467
Masatoshi Itoh <masatoshi9953@gmail.com>
2019-12-11 22:47:24 +0900

    removing unnecessary classes

--- src/main/java/jp/dressingroom/gameserver/apiguard/MainVerticle.java
+++ src/main/java/jp/dressingroom/gameserver/apiguard/MainVerticle.java
@@ -2,8 +2,6 @@ package jp.dressingroom.gameserver.apiguard;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import jp.dressingroom.gameserver.apiguard.verticle.impl.OnetimeTokenRedisImpl;
-import jp.dressingroom.gameserver.apiguard.verticle.OnetimeTokenVerticle;
 
 
 public class MainVerticle extends AbstractVerticle {
@@ -18,10 +16,6 @@ public class MainVerticle extends AbstractVerticle {
     });
     vertx.deployVerticle("jp.dressingroom.gameserver.apiguard.verticle.CryptoVerticle", res -> {
       if (res.failed()) { startPromise.fail("CryptoVerticle start failed: " + res.cause());}
-    });
-    OnetimeTokenVerticle v = new OnetimeTokenVerticle(new OnetimeTokenRedisImpl());
-    vertx.deployVerticle(v, res -> {
-      if (res.failed()) { startPromise.fail("OnetimeTokenVerticle start failed: " + res.cause());}
     });
     // delay parameter 5000 means 5,000 milliseconds( = 5sec).
     // vertx.setPeriodic(5000, id -> {System.out.println("timer fired on MainVerticle");});
