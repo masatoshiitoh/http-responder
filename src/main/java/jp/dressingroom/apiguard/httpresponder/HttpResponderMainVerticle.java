e49da8fa0f61af8911069b7d6c562df22d0a21f8
Masatoshi Itoh <masatoshi9953@gmail.com>
2019-12-23 19:58:51 +0900

    Make maven repository files with gradle.

---
+++ src/main/java/jp/dressingroom/apiguard/httpresponder/HttpResponderMainVerticle.java
@@ -0,0 +1,15 @@
+package jp.dressingroom.apiguard.httpresponder;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+
+public class HttpResponderMainVerticle extends AbstractVerticle {
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.deployVerticle("jp.dressingroom.apiguard.httpresponder.verticle.HttpServerVerticle", res -> {
+      if (res.failed()) { startPromise.fail("HttpServerVerticle start failed: " + res.cause());}
+    });
+    startPromise.complete();
+  }
+}
