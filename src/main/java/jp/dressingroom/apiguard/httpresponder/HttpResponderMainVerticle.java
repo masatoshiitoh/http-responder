2b95c2baab0088a5f29ba1c7b50fe730b915d279
Masatoshi Itoh <masatoshi9953@gmail.com>
2020-01-13 00:48:33 +0900

    Fixed #1: changed complete() location onto callback of deployVerticle.

--- src/main/java/jp/dressingroom/apiguard/httpresponder/HttpResponderMainVerticle.java
+++ src/main/java/jp/dressingroom/apiguard/httpresponder/HttpResponderMainVerticle.java
@@ -8,8 +8,11 @@ public class HttpResponderMainVerticle extends AbstractVerticle {
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     vertx.deployVerticle("jp.dressingroom.apiguard.httpresponder.verticle.HttpServerVerticle", res -> {
-      if (res.failed()) { startPromise.fail("HttpServerVerticle start failed: " + res.cause());}
+      if (res.failed()) {
+        startPromise.fail("HttpServerVerticle start failed: " + res.cause());
+      } else {
+        startPromise.complete();
+      }
     });
-    startPromise.complete();
   }
 }
